1c09f393d46b4672e38132d87600590a53ab0a52
Martin Ballester <mballester@q4tech.com>
2023-08-04 19:39:19 -0300

    results sorted

--- js/results.js
+++ js/results.js
@@ -30,6 +30,32 @@ function getLocalStorage(){
     return list;
 }
 
+function sortBy(key) {
+    records.sort(function(a, b) {
+        if (a[key] < b[key]) return -1;
+        if (a[key] > b[key]) return 1;
+        return 0;
+    });
+
+    var tabla = document.getElementById("table-history").getElementsByTagName('tbody')[0];
+    tabla.innerHTML = "";
+
+    loadTable();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    records = getLocalStorage();
+    loadTable();
+
+    var headers = document.querySelectorAll("#table-history th[data-sort]");
+    headers.forEach(function(header) {
+        header.addEventListener("click", function() {
+            var key = this.getAttribute("data-sort");
+            sortBy(key);
+        });
+    });
+});
+
 window.onload = function() {
     loadTable();
 };
