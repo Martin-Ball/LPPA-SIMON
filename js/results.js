b968069fccf508c36ea7e117599b52f3e316244e
Martin Ballester <mballester@q4tech.com>
2023-08-04 19:51:21 -0300

    fix duplicate list

--- js/results.js
+++ js/results.js
@@ -5,33 +5,32 @@ var sortColumn = null;
 function loadTable() {
     var tabla = document.getElementById("table-history").getElementsByTagName('tbody')[0];
 
-    var value = getLocalStorage();
-
-    // Limpiar tabla
     tabla.innerHTML = "";
 
-    // Ordenar los registros según la columna seleccionada
-    records.sort(function(a, b) {
-        if (sortDirection === "asc") {
-            return a[sortColumn] > b[sortColumn] ? 1 : -1;
-        } else {
-            return a[sortColumn] < b[sortColumn] ? 1 : -1;
-        }
-    });
+    if (sortColumn !== null) {
+        records.sort(function(a, b) {
+            if (sortDirection === "asc") {
+                return a[sortColumn] > b[sortColumn] ? 1 : -1;
+            } else {
+                return a[sortColumn] < b[sortColumn] ? 1 : -1;
+            }
+        });
+    }
 
-    for (var i = 0; i < value.length; i++) {
+    for (var i = 0; i < records.length; i++) {
         var row = tabla.insertRow(i);
         var nameCell = row.insertCell(0);
         var dateCell = row.insertCell(1);
         var levelCell = row.insertCell(2);
         var hitsCell = row.insertCell(3);
 
-        nameCell.innerHTML = value[i].name;
-        dateCell.innerHTML = value[i].date;
-        levelCell.innerHTML = value[i].level;
-        hitsCell.innerHTML = value[i].hits;
+        nameCell.innerHTML = records[i].name;
+        dateCell.innerHTML = records[i].date;
+        levelCell.innerHTML = records[i].level;
+        hitsCell.innerHTML = records[i].hits;
     }
 }
+
 
 function getLocalStorage(){
     var scoreLS = localStorage.getItem("score");
@@ -55,12 +54,11 @@ document.addEventListener("DOMContentLoaded", function() {
         header.addEventListener("click", function() {
             var key = this.getAttribute("data-sort");
             if (sortColumn === key) {
-                // Cambiar la dirección de ordenamiento si se hace clic nuevamente en la misma columna
                 sortDirection = sortDirection === "asc" ? "desc" : "asc";
             } else {
                 sortDirection = "asc";
-                sortColumn = key;
             }
+            sortColumn = key;
 
             loadTable();
         });
